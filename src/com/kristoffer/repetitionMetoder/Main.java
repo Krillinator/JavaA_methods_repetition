13dd96de3974b5dab62d456579bd23013246a2a6
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-09-28 13:48:01 +0200

    object and classes repetition

--- src/com/kristoffer/repetitionMetoder/Main.java
+++ src/com/kristoffer/repetitionMetoder/Main.java
@@ -3,8 +3,16 @@ package com.kristoffer.repetitionMetoder;
 public class Main {
     public static void main(String[] args) {
 
+        // Object Creation AKA Instantiation
+        Student benny = new Student();
+        Student frida = new Student();
 
+        // Set Object Values
+        benny.name = "Benny";
 
+        // Print Object Values
+        System.out.println(benny.name);
+        System.out.println(frida.name);
 
     }
 }
---
+++ src/com/kristoffer/repetitionMetoder/Student.java
@@ -0,0 +1,13 @@
+package com.kristoffer.repetitionMetoder;
+
+public class Student {
+
+    // Variables
+    public int age;
+    public String name;
+    public boolean isTired;
+
+    // Methods
+    // TODO - Create methods
+
+}
