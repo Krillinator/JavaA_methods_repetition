cf05c8382cf02c608d22d2f4f9142f6c2ecc06cf
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-09-28 15:11:13 +0200

    objects

--- src/com/kristoffer/repetitionMetoder/Main.java
+++ src/com/kristoffer/repetitionMetoder/Main.java
@@ -4,15 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
         // Object Creation AKA Instantiation
-        Student benny = new Student();
-        Student frida = new Student();
+        Student benny = new Student(15, "Benny", true);
+        Student frida = new Student(22, "Frida", false); // Var works!?
 
         // Set Object Values
-        benny.name = "Benny";
+        // benny.name = "Benny";
 
         // Print Object Values
         System.out.println(benny.name);
+        System.out.println(benny.age);
+        System.out.println(benny.isTired);
+
         System.out.println(frida.name);
+        System.out.println(frida.age);
+        System.out.println(frida.showIfSleepy());
 
     }
 }
--- src/com/kristoffer/repetitionMetoder/Student.java
+++ src/com/kristoffer/repetitionMetoder/Student.java
@@ -7,7 +7,23 @@ public class Student {
     public String name;
     public boolean isTired;
 
+    // Constructor
+    public Student(int age, String name, boolean isTired) {
+        this.age = age;
+        this.name = name;
+        this.isTired = isTired;
+    }
+
+
     // Methods
-    // TODO - Create methods
+    public String showIfSleepy() {
+
+        if (isTired) {
+            return name + " is VERY tired";
+        } else {
+            return name + " is NOT tired";
+        }
+
+    }
 
 }
