3e5df3be57c9b2fe8268dece6aff736e1dda82fc
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-09-29 10:29:20 +0200

    clean slate

--- src/com/kristoffer/repetitionMetoder/Main.java
+++ src/com/kristoffer/repetitionMetoder/Main.java
@@ -4,34 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Instantiate Objects
-        Student benny = new Student();
 
-        benny.setAge(15);
 
-        // Print age
-        System.out.println(benny.age);
-        System.out.println( test() );
-
-        // Keywords
-            // What is a 'keyword' //
-            // Primitive Data-types //
-            // for, if, switch, new //
-            // 'this' //
-
-            // Methods
-                // v-modifiers (public) //
-                // static (rule for Main) //
-                // void (return) //
-                // class //
-
-    }
-
-    // Methods
-    // v-mod, static, return-type, name, () {}
-    public static int test() {
-
-        return 2+2;
     }
 
 }
