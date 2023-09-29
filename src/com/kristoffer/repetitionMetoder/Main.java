3b2c62c214bcc6c51d1fb5bc440c338e02281d6a
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-09-29 11:18:24 +0200

    clean slate

--- src/com/kristoffer/repetitionMetoder/Main.java
+++ src/com/kristoffer/repetitionMetoder/Main.java
@@ -1,44 +1,9 @@
 package com.kristoffer.repetitionMetoder;
 
-import java.util.Objects;
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
 
-        // Loop --> infinite --> YES | NO
-        // for loop (x)     VS     do-while (1 + X)
-        // continue; what does it do?
 
-        for (int i = 0; i < 10; i++) {
-            if (i == 4) {
-                continue;
-            }
-            System.out.println("Index is: " + i);
-        }
 
-        // Instantiation
-        Scanner scanner = new Scanner(System.in);
-
-        do {
-            System.out.println("TESTING");
-            String answer = scanner.nextLine();
-
-            if (answer.equals("YES")) {
-                sayHello();
-            }
-
-            if (answer.equals("NO")) {
-                break;
-            }
-
-        } while(true);
-        System.out.println("EXITING PROGRAM - PEACEFULLY");
     }
-
-    public static void sayHello() {
-        System.out.println("WELCOME ALL PLAYERS");
-    }
-
 }
