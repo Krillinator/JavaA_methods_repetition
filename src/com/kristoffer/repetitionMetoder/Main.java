9a03cbcd296d24d8dc185d5827bfd690acd5e742
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-09-29 11:24:53 +0200

    Example

--- src/com/kristoffer/repetitionMetoder/Main.java
+++ src/com/kristoffer/repetitionMetoder/Main.java
@@ -1,9 +1,19 @@
 package com.kristoffer.repetitionMetoder;
 
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        Scanner sc = new Scanner(System.in);
 
+        do {
+            System.out.println("What's your name?");
+            TimeUnit.SECONDS.sleep(3);
+            sc.nextLine();
+
+        } while (true);
 
     }
 }
