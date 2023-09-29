08d7d199b4a3f112d2bc9e9ea88daf8c8761ab79
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-09-29 09:36:28 +0200

    example

--- src/com/kristoffer/repetitionMetoder/Main.java
+++ src/com/kristoffer/repetitionMetoder/Main.java
@@ -4,7 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Instantiate Objects
+        Student benny = new Student();
 
+        benny.setAge(15);
+
+        // Print age
+        System.out.println(benny.age);
+
+
+        // Keywords
+            // What is a 'keyword' //
+            // Primitive Data-types //
+            // for, if, switch, new //
+            // 'this' <--
+
+            // Methods
+                // v-modifiers
+                // static
+                // void
+                // class
 
     }
 }
---
+++ src/com/kristoffer/repetitionMetoder/Student.java
@@ -0,0 +1,19 @@
+package com.kristoffer.repetitionMetoder;
+
+public class Student {
+
+    int age;
+    boolean isTired;
+    String name;
+
+
+
+    public void setAge(int age) {
+
+        this.age = age;
+
+    }
+
+
+
+}
