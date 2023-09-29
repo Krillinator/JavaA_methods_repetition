2484b9c4ac2d38da36a465c2fc4863f7020190f6
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-09-29 09:14:08 +0200

    first commit

--- src/com/kristoffer/repetitionMetoder/Main.java
+++ src/com/kristoffer/repetitionMetoder/Main.java
@@ -1,28 +1,10 @@
 package com.kristoffer.repetitionMetoder;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        int[] scores = {0, 150, 500, 5000};
 
-        printScores(scores);
-        checkScoreIfZero(scores);
-    }
-
-    public static void printScores(int[] scores) {
-
-        for (int i = 0; i < scores.length; i++) {
-            System.out.println(scores[i]);
-        }
 
     }
-
-    public static void checkScoreIfZero(int[] scores) {
-        if (scores[0] == 0) {
-            System.out.println("The score was indeed 0");
-        } else {
-            System.out.println("The score was NOT 0");
-        }
-    }
-
 }
